8a7e598631253b04f8bd27cb37a8c4e5f2d6b613
LeeWongSnail <wangli_0632@163.com>
2021-10-18 23:33:22 +0800

    [comments]：Container添加BoxDecoration修饰 [jira_id]：none [version]：

--- lib/widget_learning/container_demo.dart
+++ lib/widget_learning/container_demo.dart
@@ -8,14 +8,22 @@ class ContainerDemo extends StatelessWidget {
     return Container(
       color: Colors.grey,
       child: Row(
+        // 设置主轴对齐位置
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
             child: Icon(Icons.pool, size: 24, color: Colors.white,),
-            color: Color.fromRGBO(3, 54, 255, 1),
+            // color: Color.fromRGBO(3, 54, 255, 1),
+            // 设置内边距
             padding: EdgeInsets.only(left: 30, top: 30),
-            margin: EdgeInsets.all(95.0),
+            // 设置外边距
+            margin: EdgeInsets.all(50.0),
             width: 100.0,
             height: 100.0,
+            // 可以给container设置boxdecoration
+            decoration: BoxDecoration(
+              color:  Color.fromRGBO(3, 54, 255, 1)
+            ),
           )
         ],
       ),
