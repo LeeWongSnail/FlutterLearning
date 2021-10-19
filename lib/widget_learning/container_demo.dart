3029d0cabec42eaf23889ca755cb303657a4f3e4
LeeWongSnail <wangli_0632@163.com>
2021-10-19 20:38:37 +0800

    [comments]：添加渐变效果和shape [jira_id]：none [version]：

--- lib/widget_learning/container_demo.dart
+++ lib/widget_learning/container_demo.dart
@@ -24,26 +24,37 @@ class ContainerDemo extends StatelessWidget {
             decoration: BoxDecoration(
               color:  Color.fromRGBO(3, 54, 255, 1),
               // 设置边框 可以设置所有的 也可以单独设置某一个
-              border: Border.all(
-                  color: Colors.red,
-                  width: 3.0,
-                  style: BorderStyle.solid
-              ),
+              // border: Border.all(
+              //     color: Colors.red,
+              //     width: 3.0,
+              //     style: BorderStyle.solid
+              // ),
               // 设置圆角
               // borderRadius: BorderRadius.circular(15)
-              borderRadius: BorderRadius.only(topLeft: Radius.circular(20), bottomRight: Radius.circular(30)),
-              boxShadow: [
-                BoxShadow(
-                  // 从左上角开始的偏移量
-                  offset: Offset(10.0,10.0),
-                  // 阴影的颜色
-                  color: Color.fromRGBO(16, 20, 188, 1.0),
-                  // 阴影部分的半径
-                  blurRadius: 30,
-                  // 扩散 正数表示向左下角延伸 负数表示向左上角回收
-                  spreadRadius: 10,
-                ),
-              ],
+              // borderRadius: BorderRadius.only(topLeft: Radius.circular(20), bottomRight: Radius.circular(30)),
+              // boxShadow: [
+              //   BoxShadow(
+              //     // 从左上角开始的偏移量
+              //     offset: Offset(10.0,10.0),
+              //     // 阴影的颜色
+              //     color: Color.fromRGBO(16, 20, 188, 1.0),
+              //     // 阴影部分的半径
+              //     blurRadius: 30,
+              //     // 扩散 正数表示向左下角延伸 负数表示向左上角回收
+              //     spreadRadius: 10,
+              //   ),
+              // ],
+              // 设置形状 注意这个属性的设置不可以与borderRadius同时设置
+              shape: BoxShape.circle,
+              // 渐变 这里有两种 一种Radial 一种 Linear
+              // gradient: RadialGradient(
+              //   colors: [Color.fromRGBO(255, 0, 0, 1.0), Color.fromRGBO(0, 255, 0, 1.0)]
+              // ),
+              gradient: LinearGradient(
+                  colors: [Color.fromRGBO(255, 0, 0, 1.0), Color.fromRGBO(0, 255, 0, 1.0)],
+                  begin: Alignment.topLeft,
+                  end:  Alignment.bottomRight,
+              ),
             ),
 
           )
