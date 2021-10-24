42a6ff591c97d350efd36425ead0581ea31bc224
LeeWongSnail <wangli_0632@163.com>
2021-10-24 12:50:06 +0800

    [comments]：sizebox使用 [jira_id]：none [version]：

--- lib/layout/layout_demo.dart
+++ lib/layout/layout_demo.dart
@@ -5,19 +5,53 @@ class LayoutDemo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      // 主轴的对齐方式 如果是Row则是有一个从左到右的 如果是Column则是有一个从上往下的
-      //  start 主轴开始的方向  end 主轴结束的方向
-      // spacebetween 将剩余空间在对象之间均匀放置
-      // spacearound 将剩余空间放在对象直接以及左右两边
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      // 交叉轴 交叉轴为与主轴垂直的轴
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        IconBadge(Icons.pool),
-        IconBadge(Icons.air_sharp, size: 48,),
-        IconBadge(Icons.email),
-      ],
+    // return Column(
+    //   // 主轴的对齐方式 如果是Row则是有一个从左到右的 如果是Column则是有一个从上往下的
+    //   //  start 主轴开始的方向  end 主轴结束的方向
+    //   // spacebetween 将剩余空间在对象之间均匀放置
+    //   // spacearound 将剩余空间放在对象直接以及左右两边
+    //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    //   // 交叉轴 交叉轴为与主轴垂直的轴
+    //   crossAxisAlignment: CrossAxisAlignment.start,
+    //   children: [
+    //     IconBadge(Icons.pool),
+    //     IconBadge(Icons.air_sharp, size: 48,),
+    //     IconBadge(Icons.email),
+    //   ],
+    // );
+
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          // Size Box 常用来包裹一个子控件， 默认子控件是由内部内容决定大小 但是添加了SizeBox后就可以设置为一个固定的大小
+          // 通常SizeBox也可以用来做两个空间之间的间隙
+          SizedBox(
+            height: 100,
+          ),
+          SizedBox(
+            width: 200.0,
+            height: 100.0,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.red
+              ),
+              child: Icon(Icons.ac_unit, color: Colors.white, size: 32,),
+            ),
+          ),
+          SizedBox(
+            width: 200.0,
+            height: 100.0,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.red
+              ),
+              child: Icon(Icons.ac_unit, color: Colors.white, size: 32,),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
