faf52f1c7aa1bc0b82adfa8787c8486348541422
LeeWongSnail <wangli_0632@163.com>
2022-12-03 19:11:33 +0800

    [comments]：添加stack&positioned文章 [jira_id]：none [version]：

--- Demo/flutterui/lib/main.dart
+++ Demo/flutterui/lib/main.dart
@@ -65,64 +65,20 @@ class _ProgressRouteState extends State<ProgressRoute> with SingleTickerProvider
       appBar: AppBar(
         title: Text('Box'),
       ),
-      body: Flow(
-        delegate: TestFlowDelegate(margin: EdgeInsets.all(25.0)),
-        children: [
-          Container(width: 80.0, height:80.0, color: Colors.red,),
-          Container(width: 80.0, height:80.0, color: Colors.green,),
-          Container(width: 80.0, height:80.0, color: Colors.blue,),
-          Container(width: 80.0, height:80.0,  color: Colors.yellow,),
-          Container(width: 80.0, height:80.0, color: Colors.brown,),
-
-          Container(width: 80.0, height:80.0,  color: Colors.purple,),
-        ],
+      body: ConstrainedBox(
+        constraints: BoxConstraints.expand(),
+        child: Stack(
+          alignment: Alignment.center,
+          fit: StackFit.expand,
+          clipBehavior: Clip.hardEdge,
+          children: [
+            Positioned(child: Text('I am LeeWong'), left: 20,),
+            Container(child: Text('Hello World', style: TextStyle(color: Colors.white),), color: Colors.red,),
+            Positioned(child: Text('Your Friends'), top: 20,),
+          ],
+        ),
       ),
     );
   }
 }
 
-
-class TestFlowDelegate extends FlowDelegate {
-  EdgeInsets margin;
-
-  TestFlowDelegate({this.margin = EdgeInsets.zero});
-
-  double width = 0;
-  double height = 0;
-
-  @override
-  void paintChildren(FlowPaintingContext context) {
-    // TODO: implement paintChildren
-    var x = margin.left;
-    var y = margin.top;
-    // 计算每一个子widget的位置
-    for (int i = 0; i < context.childCount; i++) {
-      var w = context.getChildSize(i)!.width + x + margin.right;
-      if(w < context.size.width) {
-        context.paintChild(i, transform: Matrix4.translationValues(x, y, 0.0));
-        x = w + margin.left;
-      } else {
-        x = margin.left;
-        y += context.getChildSize(i)!.height + margin.top + margin.bottom;
-        // 绘制子widget
-        context.paintChild(i, transform: Matrix4.translationValues(x, y, 0.0));
-        x += context.getChildSize(i)!.width + margin.left + margin.right;
-      }
-    }
-  }
-
-  @override
-  Size getSize(BoxConstraints constraints) {
-    // TODO: implement getSize
-    // 指定flow的大小，简单起见我们让宽度尽可能的大，但高度指定为200
-    // 实际开发中我们需要根据子元素所占用的具体宽高来设置flow大小
-    return Size(double.infinity, 250);
-  }
-
-  @override
-  bool shouldRepaint(covariant FlowDelegate oldDelegate) {
-    // TODO: implement shouldRepaint
-    return oldDelegate != this;
-  }
-
-}
---
+++ FlutterUI/stack&Positioned.md
@@ -0,0 +1,54 @@
+# 层叠布局
+
+层叠布局和web中的绝对定位以及移动端的frame布局相似。子组件可以根据距离父容器四个角的位置来确认自己的位置。层叠布局允许子控件按照代码声明的顺序进行堆叠(后声明的会覆盖已声明的)
+
+Flutter中使用stack和Positioned两个组件来配合实现绝对定位。
+
+## stack
+
+```dart
+Stack(
+    alignment: Alignment.center,
+    fit: StackFit.expand,
+    children: [
+      Positioned(child: Text('I am LeeWong'), left: 20,),
+      Container(child: Text('Hello World', style: TextStyle(color: Colors.white),), color: Colors.red,),
+    Positioned(child: Text('Your Friends'), top: 20,)
+  ],
+),
+```
+
+展示效果如下:
+
+![flutterui_stack]()
+
+我们来简单介绍下这几个属性:
+
+- alignment 此参数绝对如何对齐没有定位或者部分定位的子控件 共有`topLeft`、`topCenter`、`topRight`、`centerLeft`、`center`、`centerRight`、`bottomLeft`、`bottomCenter`、`bottomRight` 这几个选项
+
+- textDirection 用于确定alignment对齐的参考系。共有`ltr`和`rtl`两种，
+
+- fit 用于确定没有定位的子组件如何去适应stack的大小，共`StackFit.loose`使用子控件大小、`StackFit.expand`扩展到stack大小两个选项。
+
+- clipBehavior: 绝对对超出stack显示控件的部分如何进行裁剪,共有`none`、`hardEdge`、`antiAlias`、`antiAliasWithSaveLayer`几种选项，这里hardEdge表示直接裁剪。
+
+## Positioned
+
+positioned有left/top/right/bottom分别代表stack左、上、右、底四边的距离。width和height用于指定元素的宽度和高度。
+
+如果你想在水平方向指定子控件位置这只需要指定[left/right/width]中的两个就可以。如果三个都指定则会报错。
+
+综上我们来分析下上面代码结果的原因：
+
+- hello world文本没有指定定位，并且alignment的值为center，因此他会居中展示
+- i am leewong 只是指定了水平方向的偏移，所以垂直方向会依据alignment来判断居中展示
+- Your Friends 只是制定了垂直方向的偏移，所以水平方向会依据alignment来居中展示
+
+上面的代码我们没有设置fit参数，如果我们吧fit设置为StackFit.expand时：
+
+页面展示会变成这样
+
+![flutterui_stack]
+
+fit设置expand则默认子组件的代销扩伸到整个stack的大小。即红色底占满了屏幕 同时遮挡了后驾到父视图上的视图
+
