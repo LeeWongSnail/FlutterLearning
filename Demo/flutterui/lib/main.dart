e1ec186d65f96b2bd694a0d42a4363db88680b57
LeeWongSnail <wangli_0632@163.com>
2022-12-09 09:42:51 +0800

    [comments]：scaffold代码上传 [jira_id]：none [version]：

--- Demo/flutterui/lib/main.dart
+++ Demo/flutterui/lib/main.dart
@@ -26,105 +26,97 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: TransformPage(),
+      home: ScaffoldRoute(),
     );
   }
 }
 
-class TransformPage extends StatelessWidget {
-  const TransformPage({Key? key}) : super(key: key);
+class ScaffoldRoute extends StatefulWidget {
+  const ScaffoldRoute({Key? key}) : super(key: key);
+
+  @override
+  _ScaffoldRouteState createState() => _ScaffoldRouteState();
+}
+
+class _ScaffoldRouteState extends State<ScaffoldRoute> {
+
+  int _selectIndex = 1;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Transform'),),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Text('data'*20),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 30.0),
-            child: Row(children: [Text('xx'*30)]), //文本长度超出 Row 的最大宽度会溢出
-          ),
-          FittedBox(
-            child: Row(children: [Text('xx'*30)]),
-            // fit: BoxFit.scaleDown,
-            // clipBehavior: Clip.none,
-          ),
-          Padding(padding: EdgeInsets.symmetric(vertical: 10)),
-          // 两种不同的方式设置fit
-          Container(
-            width: 50,
-            height: 50,
-            color: Colors.red,
-            child: FittedBox(fit: BoxFit.none, child: Container(width: 60,height: 70, color: Colors.blue,),),
-          ),
-          Text('Hello World'),
-          Padding(padding: EdgeInsets.symmetric(vertical: 10)),
-          Container(
-            width: 50,
-            height: 50,
-            color: Colors.red,
-            child: FittedBox(fit: BoxFit.contain, child: Container(width: 60,height: 70, color: Colors.blue,),),
-          ),
-          Text('Hello World'),
-          Center(
-          child: Column(
-            children:  [
-              wRow(' 90000000000000000 '),
-              FittedBox(child: wRow(' 90000000000000000 ')),
-              wRow(' 800 '),
-              FittedBox(child: wRow(' 800 ')),
-            ]
-                .map((e) => Padding(
-              padding: EdgeInsets.symmetric(vertical: 20),
-              child: e,
-            ))
-                .toList(),
-          ),
-        ),
-          Padding(padding: EdgeInsets.symmetric(vertical: 10)),
-          wRow(' 90000000000000000 '),
-          SingleLineFittedBox(child: wRow(' 90000000000000000 ')),
-          wRow(' 800 '),
-          SingleLineFittedBox(child: wRow(' 800 ')),
-      ],
+      appBar: AppBar(
+        title: Text('Scaffold'),
+        actions: [
+          IconButton(onPressed: () {
+          }, icon: Icon(Icons.share)),
+        ],
+        leading: Builder(builder: (context) {
+          return IconButton(onPressed: () {
+            Scaffold.of(context).openDrawer();
+          }, icon: Icon(Icons.dashboard, color: Colors.white,));
+        },),
+        backgroundColor: Colors.yellow,
+        bottom: ,
+      ),
+      drawer: CustomDrawer(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.air), label: 'Air'),
+          BottomNavigationBarItem(icon: Icon(Icons.school), label: 'School'),
+          BottomNavigationBarItem(icon: Icon(Icons.library_music), label: 'Music')
+        ],
+        currentIndex: _selectIndex,
+        // fixedColor: Colors.blue,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: _onAdd,
       ),
     );
   }
 
-  // 直接使用Row
-  Widget wRow(String text) {
-    Widget child = Text(text);
-    child = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [child, child, child],
-    );
-    return child;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectIndex = index;
+    });
+  }
+
+  void _onAdd() {
+
   }
 }
 
-class SingleLineFittedBox extends StatelessWidget {
-  const SingleLineFittedBox({Key? key,this.child}) : super(key: key);
-  final Widget? child;
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (_, constraints) {
-        return FittedBox(
-          child: ConstrainedBox(
-            constraints: constraints.copyWith(
-              //让 maxWidth 使用屏幕宽度
-                minWidth: constraints.maxWidth,
-              maxWidth: double.infinity,
-            ),
-            child: child,
+    return Drawer(
+      child: MediaQuery.removePadding(context: context, child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(padding: EdgeInsets.only(top: 38), child:  Row(
+            children: [
+              Padding(padding: EdgeInsets.symmetric(horizontal: 16), child: ClipOval(
+                child: Image.asset('images/icon.webp', width: 80,),
+              ),),
+              Text('LeeWong', style: TextStyle(fontWeight: FontWeight.bold),)
+            ],
           ),
-        );
-      },
+          ),
+          Expanded(child: ListView(children: [
+            ListTile(leading: Icon(Icons.add), title: Text('Add Account'),),
+            ListTile(leading: Icon(Icons.settings), title: Text('Manager Account'),),
+          ],))
+        ],
+      ),
+      ),
     );
   }
 }
-
 
