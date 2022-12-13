445472d74a8c54fb40ab16bd8ef9b29ffada7206
LeeWongSnail <wangli_0632@163.com>
2022-12-13 23:33:40 +0800

    [comments]：滚动监听学习 [jira_id]：none [version]：

--- Demo/flutterui/lib/main.dart
+++ Demo/flutterui/lib/main.dart
@@ -27,7 +27,61 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: ScrollControllerTestRoute(),
+    );
+  }
+}
+
+class ScrollControllerTestRoute extends StatefulWidget {
+  const ScrollControllerTestRoute({Key? key}) : super(key: key);
+
+  @override
+  _ScrollControllerTestRouteState createState() => _ScrollControllerTestRouteState();
+}
+
+class _ScrollControllerTestRouteState extends State<ScrollControllerTestRoute> {
+
+  ScrollController _scrollController = ScrollController();
+
+  // 是否展示回到顶部按钮
+  bool showToTopBtn = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _scrollController.addListener(() {
+      print(_scrollController.offset);
+      if (_scrollController.offset < 1000 && showToTopBtn) {
+        setState(() {
+          showToTopBtn = false;
+        });
+      } else if (_scrollController.offset >= 1000 && showToTopBtn) {
+        setState(() {
+          showToTopBtn = true;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('滚动监听'),),
+      body: Scrollbar(
+        child: ListView.builder(itemBuilder: (context, index){
+          return ListTile(title: Text('$index'),);
+        }, itemCount: 100,itemExtent: 50, controller: _scrollController,),
+      ),
+      floatingActionButton: !showToTopBtn ? null : FloatingActionButton(onPressed: (){
+        _scrollController.animateTo(.0, duration: Duration(milliseconds: 200), curve: Curves.ease);
+      }, child: Icon(Icons.arrow_upward),),
     );
   }
 }
