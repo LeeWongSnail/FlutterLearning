35b4e864e1991d36a52b74760569d0702e457a90
LeeWongSnail <wangli_0632@163.com>
2022-12-23 21:58:12 +0800

    [comments]：添加pageview学习 [jira_id]：none [version]：

--- Demo/flutterui/lib/main.dart
+++ Demo/flutterui/lib/main.dart
@@ -27,142 +27,46 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: InfiniteGridView(),
+      home: PagingView(),
+    );
+  }
+}
+
+class PagingView extends StatefulWidget {
+  const PagingView({Key? key}) : super(key: key);
+
+  @override
+  _PagingViewState createState() => _PagingViewState();
+}
+
+class _PagingViewState extends State<PagingView> {
+  @override
+  Widget build(BuildContext context) {
+
+    var children = <Widget>[];
+    for(int i = 0; i < 6; i++) {
+      children.add(Page(text: '${i}'));
+    }
+
+    return Scaffold(appBar: AppBar(title: Text('PageView'),),
+      body: PageView(children: children,scrollDirection: Axis.vertical,allowImplicitScrolling: true,),
     );
   }
 }
 
 
-class SliverGridDelegateCrossAxisExtent extends StatefulWidget {
-  const SliverGridDelegateCrossAxisExtent({Key? key}) : super(key: key);
 
+class Page extends StatefulWidget {
+  const Page({Key? key, required this.text}) : super(key: key);
+  final String text;
   @override
-  _SliverGridDelegateCrossAxisExtentState createState() => _SliverGridDelegateCrossAxisExtentState();
+  _PageState createState() => _PageState();
 }
 
-class _SliverGridDelegateCrossAxisExtentState extends State<SliverGridDelegateCrossAxisExtent> {
+class _PageState extends State<Page> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('GridView'),),
-      body: GridView.extent(
-        maxCrossAxisExtent: 120.0,
-        childAspectRatio: 2.0,
-        children: <Widget>[
-          Icon(Icons.ac_unit),
-          Icon(Icons.airport_shuttle),
-          Icon(Icons.all_inclusive),
-          Icon(Icons.beach_access),
-          Icon(Icons.cake),
-          Icon(Icons.free_breakfast),
-        ],
-      ),);
+    print('build ${widget.text}');
+    return Center(child: Text("${widget.text}", textScaleFactor: 5,),);
   }
 }
-
-
-class InfiniteGridView extends StatefulWidget {
-  const InfiniteGridView({Key? key}) : super(key: key);
-
-  @override
-  _InfiniteGridViewState createState() => _InfiniteGridViewState();
-}
-
-class _InfiniteGridViewState extends State<InfiniteGridView> {
-
-  List<IconData> _icons = []; // 保存icon数据
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _retrieveIcons();
-  }
-
-  void _retrieveIcons() {
-   Future.delayed(Duration(milliseconds: 200)).then((e){
-     setState(() {
-       _icons.addAll([
-         Icons.ac_unit,
-         Icons.airport_shuttle,
-         Icons.all_inclusive,
-         Icons.beach_access,
-         Icons.cake,
-         Icons.free_breakfast,
-       ]);
-     });
-   }) ;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('GridView'),),
-      body: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3, // 每行三列
-        childAspectRatio: 1.0 // 宽高比
-      ), itemBuilder: (context, index) {
-        if(index == _icons.length - 1 && _icons.length < 200) {
-          _retrieveIcons();
-        }
-        return Icon(_icons[index]);
-      }, itemCount: _icons.length,),
-    );
-  }
-}
-
-
-class GrideViewRoute extends StatefulWidget {
-  const GrideViewRoute({Key? key}) : super(key: key);
-
-  @override
-  _GrideViewRouteState createState() => _GrideViewRouteState();
-}
-
-class _GrideViewRouteState extends State<GrideViewRoute> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('GridView'),),
-        body: GridView(children: [
-          Icon(Icons.ac_unit),
-          Icon(Icons.airport_shuttle),
-          Icon(Icons.all_inclusive),
-          Icon(Icons.beach_access),
-          Icon(Icons.cake),
-          Icon(Icons.free_breakfast,color: Colors.red,)
-        ],gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3, // 横轴展示的widget个数
-          mainAxisSpacing: 20, //主轴方向的间距。
-          childAspectRatio: 1.0, // 子元素在横轴长度和主轴长度的比例。
-          crossAxisSpacing: 5, // 横轴方向子元素的间距。
-        ),)
-      );
-  }
-}
-
-class GridViewCountRoute extends StatefulWidget {
-  const GridViewCountRoute({Key? key}) : super(key: key);
-
-  @override
-  _GridViewCountRouteState createState() => _GridViewCountRouteState();
-}
-
-class _GridViewCountRouteState extends State<GridViewCountRoute> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('GridViewCount'),),
-      body: GridView.count(
-        crossAxisCount: 3,
-        childAspectRatio: 1.0,
-        children: <Widget>[
-          Icon(Icons.ac_unit),
-          Icon(Icons.airport_shuttle),
-          Icon(Icons.all_inclusive),
-          Icon(Icons.beach_access),
-          Icon(Icons.cake),
-          Icon(Icons.free_breakfast),
-        ],
-      ),
-    );
-  }
-}
-
